3fdf36f96cd708a420dc492655d8591389c911a0
Lautaro Bestard <lautarobestard@gmail.com>
2019-04-27 23:08:48 -0300

    v2

--- scripts.js
+++ scripts.js
@@ -1,22 +1,33 @@
 /*jshint esversion: 6*/
 
-function initGimmiCoords(){
-  var Mapa = cargarMapa();  // Cargo Mapa
-      editorDePoligonos = drawingMan(Mapa);
-      Geocoder = new google.maps.Geocoder();
-      nuevoPoligono = document.getElementById('nuevoPoly');
+function initGimmiCoords() {
+  var Mapa = cargarMapa(); // Cargo Mapa
+  editorDePoligonos = drawingMan(Mapa);
 
-  google.maps.event.addListener(editorDePoligonos, 'overlaycomplete', function(polygon) {
+  Geocoder = new google.maps.Geocoder();
+  nuevoPoligono = document.getElementById('nuevoPoly');
+
+  google.maps.event.addListener(editorDePoligonos, 'overlaycomplete', function (polygon) {
     // guardo coordenadas del poligono
-    var coordenadasGuardadas = polygon.overlay.getPath().getArray();
-    mostrarCoordenadas(coordenadasGuardadas);
+    var newShape = polygon.overlay;
+      newShape.type = polygon.type;
+
+    google.maps.event.addListener(newShape, 'click', function (e) {
+      var coordenadasGuardadas = polygon.overlay.getPath().getArray();
+      mostrarCoordenadas(coordenadasGuardadas);
+    })
+      
   });
+
 }
 
 // FUNCION PARA INICIAR EL MAPA
 function cargarMapa() {
   var Mapa = new google.maps.Map(document.getElementById('map'), {
-    center: {lat:-34.618356, lng:-58.433464},
+    center: {
+      lat: -34.618356,
+      lng: -58.433464
+    },
     zoom: 12,
     mapTypeId: google.maps.MapTypeId.ROADMAP
   });
@@ -24,23 +35,25 @@ function cargarMapa() {
 }
 
 // FUNCION PARA DIBUJAR POLIGONOS
-function drawingMan(map){
-    var drawingManager = new google.maps.drawing.DrawingManager({
-      drawingMode: google.maps.drawing.OverlayType.POLYGON,
-      drawingControl: true,
-      drawingControlOptions: {
-        position: google.maps.ControlPosition.TOP_CENTER,
-        drawingModes: 'polygon',
-        polygonOptions: {
-          editable: true,
-        },
-      },
-    });
-    drawingManager.setMap(map);
-    return drawingManager;
+function drawingMan(map) {
+  var drawingManager = new google.maps.drawing.DrawingManager({
+    drawingMode: google.maps.drawing.OverlayType.POLYGON,
+    drawingControl: true,
+    drawingControlOptions: {
+      position: google.maps.ControlPosition.TOP_CENTER,
+      drawingModes: [google.maps.drawing.OverlayType.POLYGON],
+    },
+    polygonOptions: {
+      clickable: true,
+      editable: true,
+    },
+  });
+  drawingManager.setMap(map);
+
+  return drawingManager;
 }
 
-function mostrarCoordenadas(coordenadas){
+function mostrarCoordenadas(coordenadas) {
   // creo elemento
   var coords = document.createElement("table");
   // lo lleno con las coordenadas
